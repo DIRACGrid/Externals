e59353c661d13038f1e9b5c10648d1d05438a0f3
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2016-02-01 14:54:21 +0100

    Output directories adjusted

--- MySQL/dirac-make.py
+++ MySQL/dirac-make.py
@@ -22,9 +22,9 @@ versions = { 'mysql' : "5.6.23" }
 
 ch.setPackageVersions( versions )
 
-dataDir = os.path.join( os.path.dirname( ch.getPrefix() ), "mysql" )
-if not os.path.isdir( dataDir ):
-  os.makedirs( dataDir )
+shareDir = os.path.join( os.path.dirname( ch.getPrefix() ), "mysql" )
+if not os.path.isdir( shareDir ):
+  os.makedirs( shareDir )
 
 trashDir = os.path.join( os.path.dirname( ch.getPrefix() ), "trash" )
 if not os.path.isdir( trashDir ):
@@ -32,9 +32,9 @@ if not os.path.isdir( trashDir ):
 
 configureArgs = []
 configureArgs.append( "-DCMAKE_INSTALL_PREFIX='%s'" % ch.getPrefix() )
-configureArgs.append( "-DINSTALL_SUPPORTFILESDIR='%s/share/mysql'" % dataDir )
+configureArgs.append( "-DINSTALL_SUPPORTFILESDIR='%s/share/mysql'" % shareDir )
 
-# We do not need thos
+# We do not need those
 configureArgs.append( "-DINSTALL_MYSQLTESTDIR='%s'" % trashDir )
 configureArgs.append( "-DINSTALL_INFODIR='%s'" % trashDir )
 configureArgs.append( "-DINSTALL_DOCDIR='%s'" % trashDir )
@@ -42,6 +42,8 @@ configureArgs.append( "-DINSTALL_DOCREADMEDIR='%s'" % trashDir )
 configureArgs.append( "-DINSTALL_MANDIR='%s'" % trashDir )
 
 configureArgs.append( "-DWITH_EMBEDDED_SERVER=0" )
+# And if we do not want the server part
+# configureArgs.append( "-DWITHOUT_SERVER=1" )
 
 if not ch.deployPackage( "mysql",
                          configureExecutable = "/usr/bin/cmake",
@@ -49,7 +51,7 @@ if not ch.deployPackage( "mysql",
   ch.ERROR( "Could not deploy package mysql" )
   sys.exit( 1 )
 
-etcDir = os.path.join( dataDir, "etc" )
+etcDir = os.path.join( shareDir, "etc" )
 if not os.path.isdir( etcDir ):
   os.makedirs( etcDir )
 

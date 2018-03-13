27a9d281030835080fccc8ede3488fe260b6d5b4
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2018-03-13 19:05:10 +0100

    Bug fix

--- SOAP/dirac-make.py
+++ SOAP/dirac-make.py
@@ -23,7 +23,7 @@ versions = { 'suds-jurko' : "0.6",
 ch.setPackageVersions( versions )
 
 for package, version in versions.iteritems():
-  packageToInstall = "%s=%s" % (package, version)
+  packageToInstall = "%s==%s" % (package, version)
   if not ch.pip( packageToInstall ):
     logging.error( "Could not deploy %s with pip", package )
     sys.exit( 1 )
